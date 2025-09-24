2f74773605ad6a3202b881d05083ac7ff92f0355
Golias349 <81453401+Golias349@users.noreply.github.com>
2025-09-24 06:38:36 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,1 @@
-const CACHE_NAME="grao-digital-cache-v"+new Date().getTime();
-const URLS=["./","./index.html","./style.css","./app.js","./manifest.json","./icon.png"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(URLS)))});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>{if(k!==CACHE_NAME)return caches.delete(k);}))))});
-self.addEventListener("fetch",e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
+// Service worker com cache + suporte a notificações

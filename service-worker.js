c2df4833bd8398357bd6555bdc3b128d53d0bd14
Golias349 <81453401+Golias349@users.noreply.github.com>
2025-09-27 13:54:17 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,8 @@
-
-const CACHE_NAME = 'grao-digital-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './estilo.css',
-  './app.js',
-  './manifest.json',
-  './icone-192.png',
-  './icone-512.png'
-];
-
-self.addEventListener('install', (e)=>{
-  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));
+self.addEventListener("install",e=>{
+  e.waitUntil(caches.open("grao-digital").then(c=>c.addAll([
+    "./","index.html","estilo.css","app.js","manifest.json"
+  ])));
 });
-
-self.addEventListener('activate', (e)=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));
-});
-
-self.addEventListener('fetch', (e)=>{
-  const url = new URL(e.request.url);
-  if(url.origin === location.origin){
-    e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
-  }
+self.addEventListener("fetch",e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });

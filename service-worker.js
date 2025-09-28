734fde36a502031c3cbc38d983bb0938eb67bc91
Golias349 <81453401+Golias349@users.noreply.github.com>
2025-09-28 09:07:28 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,1 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('grao-digital').then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/estilo.css',
-        '/app.js',
-        '/manifest.json'
-      ]);
-    })
-  );
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
-  );
-});
+self.addEventListener('install', e => { console.log('Service Worker instalado'); });
